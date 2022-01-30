9c95d85f36f591c458034f618a33fb3d0c0e88b4
sanin-mohd <sanin.nitcalicut.ece@gmail.com>
2022-01-30 22:56:27 +0530

    project renamed

--- onlinE_api/onlinE_api/wsgi.py
+++ onlinE_api/onlinE_api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for teachme_api project.
+WSGI config for onlinE_api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teachme_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlinE_api.settings')
 
 application = get_wsgi_application()
